c47bf3c99cf7a34ff5133928947835fbe420dd3f
danghoanghai2k2 <danghoanghai2k2@gmail.com>
2024-06-07 23:58:46 +0700

    AddDatat

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'FirstAnGuLar' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('FirstAnGuLar');
+    expect(app.lists).toEqual('FirstAnGuLar');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { User } from './common/User';
+import { HeaderComponent } from './component/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'FirstAnGuLar';
+  // tạo ra thẻ tiêu đề
+  lists = 'Danh sách các bạn học sinh';
+  nguoiDung:User = {
+    id :1,
+    name :'đặng thanh hải',
+    email :'hai@gmail.com', 
+    address: 'Tiền Hải - Thái Bình'
+  };
 }
